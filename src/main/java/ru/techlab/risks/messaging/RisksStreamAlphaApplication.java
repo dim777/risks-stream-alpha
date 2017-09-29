715b6ae192b1b0290cd16dc8494144fb058a229a
dim777 <dim777@ya.ru>
2017-09-29 08:20:33 +0300

    Init

---
+++ src/main/java/ru/techlab/risks/messaging/RisksStreamAlphaApplication.java
@@ -0,0 +1,12 @@
+package ru.techlab.risks.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisksStreamAlphaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisksStreamAlphaApplication.class, args);
+	}
+}
